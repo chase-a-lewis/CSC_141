a10d8c467ed7168522fa9f937d1fda9cffb7d1f0
chase-a-lewis <chase.lewis001@albright.edu>
2026-09-10 13:32:56 -0400

    commit

--- assignments/01_getting_started/01_01_python_org.py
+++ assignments/01_getting_started/01_01_python_org.py
@@ -1,2 +1,6 @@
-print("Hello Python world!")
-#comment
+'''
+    I have learned coding in HTML, CSS, and JavaScript and I only took one class in high school and I enjoyed it. So I've had 
+some experience with computer programming before. This is my first time learning python. I'm taking this course because it was 
+required for my major, which is computer science, but I also enjoy learning to code. I want to learn more about this type of 
+stuff because I find it interesting! I'm excited to see what I can achieve in this class by the end of the semester! 
+'''
